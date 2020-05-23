6f86b06fc2bbf83a7a814be8d5100a112b33cad7
Akshay Patel <ak811226@dal.ca>
2020-05-23 01:01:46 -0300

    Initial commit

---
+++ src/main/java/com/example/CATME/CatmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CATME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CATME/CatmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CATME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
