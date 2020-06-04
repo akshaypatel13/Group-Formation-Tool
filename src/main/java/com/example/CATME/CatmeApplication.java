12010bc39d73272d30b7acfc8310441a7221e158
Akash Bharti <ak420492@dal.ca>
2020-06-04 15:02:26 -0300

    Revert "Merge branch 'feature/dataSourceConfig' into 'master'"
    This reverts merge request !21

--- src/main/java/com/example/CATME/CatmeApplication.java
+++ src/main/java/com/example/CATME/CatmeApplication.java
@@ -2,13 +2,12 @@ package com.example.CATME;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class CatmeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatmeApplication.class, args);
-		System.out.print("Hello Hi");
 	}
+
 }
--- src/main/java/com/example/CATME/Instructor/InstructorController.java
+++
@@ -1,9 +0,0 @@
-package com.example.CATME.Instructor;
-
-import com.example.CATME.database.UserSignUpDB;
-
-public interface InstructorController {
-
-    boolean addStudents(String[] details, String password, int courseID, String courseName, UserSignUpDB userService);
-}
-
