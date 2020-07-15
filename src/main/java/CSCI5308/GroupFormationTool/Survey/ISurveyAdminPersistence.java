a1f1e18fb505dedee6b8d84ef23acc295fed711a
Ruize Nie <rz376775@dal.ca>
2020-07-15 18:35:34 -0300

    refactor for response package

--- src/main/java/CSCI5308/GroupFormationTool/Survey/ISurveyAdminPersistence.java
+++ src/main/java/CSCI5308/GroupFormationTool/Survey/ISurveyAdminPersistence.java
@@ -11,5 +11,4 @@ public interface ISurveyAdminPersistence
     public boolean deleteSurveyQuestion(long questionID);
     public boolean publishSurvey(long courseID, long groupSize);
     public boolean disableSurvey(long courseID);
-	public List<Question> sortQuestionByDateCreated(List<Question> quesionsWithoutOptions, List<Question> quesionsWithOptions);
 }
---
+++ src/test/java/CSCI5308/GroupFormationTool/Response/ReponseTest.java
@@ -0,0 +1,92 @@
+package CSCI5308.GroupFormationTool.Response;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import CSCI5308.GroupFormationTool.QuestionManager.IQuestion;
+import CSCI5308.GroupFormationTool.QuestionManager.IQuestionPersistence;
+import CSCI5308.GroupFormationTool.QuestionManager.OptionValue;
+import CSCI5308.GroupFormationTool.QuestionManager.Question;
+import CSCI5308.GroupFormationTool.QuestionManager.QuestionDBMock;
+
+@SuppressWarnings("deprecation")
+public class ReponseTest {
+	
+	@Test
+	public void ConstructorTests()
+	{
+		Response response = new Response();
+		Assert.isNull(response.getId());
+		Assert.isTrue(response.getResponse().isEmpty());
+
+	}
+	
+	@Test
+	public void getId() 
+	{
+		Response response = new Response();
+		response.setId(7);
+		Assert.isTrue(response.getId() == 7);
+	}
+	
+	@Test
+	public void setId() 
+	{
+		Response response = new Response();
+		response.setId(7);
+		Assert.isTrue(response.getId() == 7);
+	}
+	
+	@Test
+	public void getResponse() 
+	{
+		Response response = new Response();
+		response.setResponse("Here is the answer");
+		Assert.isTrue(response.getResponse().equals("Here is the answer"));
+	}
+	
+	@Test
+	public void setResponse() 
+	{
+		Response response = new Response();
+		response.setResponse("Here is the answer");
+		Assert.isTrue(response.getResponse().equals("Here is the answer"));
+	}
+	
+	@Test
+	public void sortQuestionByDateCreated() {
+		
+		List<IQuestion> questions1 = new ArrayList<>();
+		List<IQuestion> questions2 = new ArrayList<>();
+		
+		Question question1 = new Question();
+		question1.setTimestamp(Timestamp.valueOf("2020-06-16 00:00:00"));
+
+		Question question2 = new Question();
+		question2.setTimestamp(Timestamp.valueOf("2020-06-26 00:00:00"));
+		
+		questions1.add(question1);
+		questions2.add(question2);
+		
+		Response response = new Response();
+		List<IQuestion> result = response.sortQuestionByDateCreated(questions1, questions2);
+		Assert.isTrue(result.indexOf(question1) == 0);
+	}
+	
+	@Test
+	public void saveResponseAnswer() {
+		
+		HashMap<String, String> answer = new HashMap<>();
+		answer.put("Question", "Response");
+
+		IResponsePersistence responseDB = new ResponseDBMock();
+		boolean status = responseDB.saveResponse(answer, "B-000000");
+		Assert.isTrue(status == true);
+
+	}
+}
