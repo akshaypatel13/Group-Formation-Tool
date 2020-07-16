560e7ec0cd5088e214a6bd120342d9b31fa0eea2
Manpreet Singh <mn407919@dal.ca>
2020-07-16 17:26:32 -0300

    tEST

--- src/main/java/CSCI5308/GroupFormationTool/QuestionManager/Options.java
+++ src/main/java/CSCI5308/GroupFormationTool/QuestionManager/Options.java
@@ -3,58 +3,47 @@ package CSCI5308.GroupFormationTool.QuestionManager;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Options implements IOptions
-{
+public class Options implements IOptions {
 	public List<OptionValue> optionList;
-	
-	public Options() 
-	{
+
+	public Options() {
 		setDefault();
 	}
-	
-	public void setDefault()
-	{
+
+	public void setDefault() {
 		optionList = new ArrayList<>();
 	}
-	
-	public List<OptionValue> getOptionList()
-	{
+
+	public List<OptionValue> getOptionList() {
 		return optionList;
 	}
 
-	public void setOptionList(List<OptionValue> optionList)
-	{
+	public void setOptionList(List<OptionValue> optionList) {
 		this.optionList = optionList;
 	}
 
-	public void addOption() 
-	{
-		String index = String.valueOf(optionList.size()+1);
-		///////////////////////////////////////////////////
-		optionList.add(QuestionManagerAbstractFactory.instance().createOptionValueParamInstance(index,index));
+	public void addOption() {
+		String index = String.valueOf(optionList.size() + 1);
+		optionList.add(QuestionManagerAbstractFactory.instance().createOptionValueParamInstance(index, index));
 	}
-	
-	public void saveOptions(IQuestionPersistence questionDB, long questionID) 
-	{
-		int order=1;
-		if(questionID!=-1) {
-			for (OptionValue option: optionList)
-			{
+
+	public void saveOptions(IQuestionPersistence questionDB, long questionID) {
+		int order = 1;
+		if (questionID != -1) {
+			for (OptionValue option : optionList) {
 				String text = option.getText();
-		        String storedAs = option.getStoredAs();
-		        if(isStringEmpty(text) || isStringEmpty(storedAs)) 
-		        {
-		        	continue;
-		        }
-		        questionDB.createQuestionOption(option, order++, questionID);
+				String storedAs = option.getStoredAs();
+				if (isStringEmpty(text) || isStringEmpty(storedAs)) {
+					continue;
+				}
+				questionDB.createQuestionOption(option, order++, questionID);
 			}
 		}
-				
+
 	}
-	
-	public boolean isStringEmpty(String s) 
-	{
-		return s.replaceAll(" ","").length() == 0;
+
+	public boolean isStringEmpty(String s) {
+		return s.replaceAll(" ", "").length() == 0;
 	}
-	
+
 }
